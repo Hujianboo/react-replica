eb778411b048a15c199548ed1d5b56272ee6314a
Hujianboo <466636709@qq.com>
2023-01-21 01:12:25 +0800

    feat: add jsx

---
+++ packages/react/src/jsx.ts
@@ -0,0 +1,93 @@
+import { REACT_ELEMENT_TYPE } from 'shared/ReactSymbol';
+import type {
+	ReactElementType,
+	Type,
+	Key,
+	Ref,
+	Props
+} from 'shared/ReactTypes';
+
+const ReactElement = (
+	type: Type,
+	key: Key,
+	ref: Ref,
+	props: Props
+): ReactElementType => {
+	const element = {
+		$$typeof: REACT_ELEMENT_TYPE,
+		key,
+		type,
+		ref,
+		props,
+		__mark: 'bobo'
+	};
+	return element;
+};
+// 创建返回的ReactElement对象
+export const jsx = (type: Type, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+};
+
+export const jsxDEV = (type: Type, config: any, ...maybeChildren: any) => {
+	let key: Key = null;
+	const props: Props = {};
+	let ref: Ref = null;
+
+	for (const prop in config) {
+		const val = config[prop];
+		if (prop === 'key') {
+			if (val !== undefined) {
+				key = '' + val;
+			}
+			continue;
+		}
+		if (prop === 'ref') {
+			if (val !== undefined) {
+				ref = val;
+			}
+			continue;
+		}
+		if ({}.hasOwnProperty.call(config, prop)) {
+			props[prop] = val;
+		}
+	}
+	const maybeChildrenLength = maybeChildren.length;
+	if (maybeChildrenLength) {
+		if (maybeChildrenLength === 1) {
+			props.children = maybeChildren[0];
+		} else {
+			props.children = maybeChildren;
+		}
+	}
+	return ReactElement(type, key, ref, props);
+};
